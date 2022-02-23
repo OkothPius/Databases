d685d1e1a27716d4150697f5393b6282e5d59cec
Okoth Pius <orukopius8@gmail.com>
2022-02-23 16:01:57 +0300

    [Feat] mv

--- sqlalchemycore-storedproc.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy import Table, MetaData
-
-# engine = create_engine('postgres://postgres:password@localhost/red30')
-engine = create_engine('postgresql://redruko:password@localhost/red30')
-
-with engine.connect() as connection:
-	meta = MetaData(engine)
-	sales_table = Table('sales', meta, autoload=True, autoload_with=engine)
